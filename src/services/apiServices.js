9b46935f7909495e965be1e8cd4a6c87549b295c
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2025-01-02 13:05:01 +0000

    feat: adição de dependências-> dotenv, axios e express.js

---
+++ src/services/apiServices.js
@@ -0,0 +1,7 @@
+//Criar serviço de integração Externa
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config(); //Carregar variáveis de ambiente
+
+const API_URL = "api.openweathermap.org";
